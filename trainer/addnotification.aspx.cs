1c92f4c0568e3cb09cbc623698f37bc3e9170e3b
teamhexatech <44142334+teamhexatech@users.noreply.github.com>
2018-11-27 02:02:33 +0530

    Update trainer/addnotification.aspx.cs

---
+++ trainer/addnotification.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+public partial class trainer_addnotificationstud : System.Web.UI.Page
+{
+    connection c = new connection();
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        string str = "insert into stnotifications values('" + date.Text + "','" + title.Text + "','" + content.Text + "')";
+        c.con.Open();
+        SqlCommand cmd = new SqlCommand(str, c.con);
+        cmd.ExecuteNonQuery();
+        Response.Write("<script>aleart('saved')</script>");
+        Response.Redirect("addnotification.aspx");
+        c.con.Close();
+
+
+    }
+}
