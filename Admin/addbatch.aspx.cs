7d985e8d74c874e6bd595d2f465267fba5121c32
teamhexatech <44142334+teamhexatech@users.noreply.github.com>
2018-11-27 01:57:41 +0530

    Update Admin/addbatch.aspx.cs

---
+++ Admin/addbatch.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+
+
+public partial class Admin_addbatch : System.Web.UI.Page
+{
+    connection c = new connection();
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (Session["admin"] == null)
+        {
+            Response.Redirect("~/index.aspx");
+        }
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        string str = "insert into batch values('" + course.SelectedItem.Value + "','" + time.Text + "')";
+        c.con.Open();
+        SqlCommand cmd = new SqlCommand(str, c.con);
+        cmd.ExecuteNonQuery();
+
+        Response.Write("<script>alert('Batch Details Added')</script>");
+        Server.Transfer("addbatch.aspx");
+
+    
+        c.con.Close();
+    }
+}
