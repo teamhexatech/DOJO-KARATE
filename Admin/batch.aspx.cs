54fdc4453e4fcbddad2c7990e73ddc9b1aab66f9
teamhexatech <44142334+teamhexatech@users.noreply.github.com>
2018-11-27 01:58:50 +0530

    Update Admin/batch.aspx.cs

---
+++ Admin/batch.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+public partial class Admin_batch : System.Web.UI.Page
+{
+    connection c = new connection();
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (Session["admin"] == null)
+        {
+            Response.Redirect("~/index.aspx");
+        }
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+       
+    }
+}
