9edc475d3c72b5a0150929d22a3670df10ee438e
teamhexatech <44142334+teamhexatech@users.noreply.github.com>
2018-11-27 02:02:22 +0530

    Update trainer/addstudent.aspx.cs

---
+++ trainer/addstudent.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+
+public partial class trainer_addstudent : System.Web.UI.Page
+{
+    connection c = new connection();
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        string str = "insert into student values('" + fname.Text + "','" + lname.Text + "','" + gender.SelectedItem.Value + "','" + dob.Text + "','" + address.Text + "','" + email.Text + "','" + mob.Text + "','" + course.SelectedItem.Value + "','" + batch.SelectedItem.Value + "','" + password.Text + "')";
+        c.con.Open();
+        SqlCommand cmd = new SqlCommand(str, c.con);
+        cmd.ExecuteNonQuery();
+        Response.Write("<script>aleart('saved')</script>");
+        Server.Transfer("student.aspx");
+        c.con.Close();
+    }
+}
