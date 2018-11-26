46c47735920bfa31dd26fc95ca154b93727e4226
teamhexatech <44142334+teamhexatech@users.noreply.github.com>
2018-11-27 02:02:25 +0530

    Update trainer/addtournament.aspx.cs

---
+++ trainer/addtournament.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+
+public partial class trainer_addtournament : System.Web.UI.Page
+{
+    connection c = new connection();
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        string str = "insert into tournament values('" + tname.Text + "','" + time.Text + "','" + date.Text + "','" + location.Text + "','" + rules.Text + "')";
+        c.con.Open();
+        SqlCommand cmd = new SqlCommand(str, c.con);
+        cmd.ExecuteNonQuery();
+        Response.Write("<script>aleart('saved')</script>");
+        Response.Redirect("Add_tournament.aspx");
+        c.con.Close();
+    }
+}
